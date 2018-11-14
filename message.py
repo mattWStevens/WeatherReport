54a54968d0071b512741cf47f4fb5a53879e58d3
mattWStevens <42496928+mattWStevens@users.noreply.github.com>
2018-11-14 09:59:42 -0500

    contains the class for an EmailMessage

---
+++ message.py
@@ -0,0 +1,26 @@
+# message.py
+# author: Matthew Stevens
+
+"""
+This is a class representing a neatly formatted
+message to be used for the body of an email.
+"""
+
+class EmailMessage:
+	sent_from = ""
+	to = ""
+	subject = ""
+	body = "" 
+
+	# initializes the EmailMessage
+	def __init__(self, sent_from, to, subject, body):
+		self.sent_from = sent_from
+		self.to = to
+		self.subject = subject
+		self.body = body
+	
+	# returns the EmailMessage
+	def to_string(self):
+		message = "Subject: {}\n\n{}".format(self.subject, self.body)
+
+		return message
